c08c57dd776601e0083ff17553a388a3b2ac04a7
Tim Yau <tyautl@nexify.com.hk>
2020-11-22 10:58:57 +0800

    Add plugin

---
+++ src/components/nx-form/nx-form.plugin.js
@@ -0,0 +1,15 @@
+import NxForm from "./NxForm";
+import NxFormField from "./NxFormField";
+import FormulatePlugins from "@/components/formulate/formulate-plugins";
+
+const NxFormPlugin = {
+  install(Vue) {
+    Vue.component("nx-form", NxForm);
+    Vue.component("nx-form-field", NxFormField);
+    Vue.use(VueFormulate, {
+      plugins: [FormulatePlugins],
+    });
+  },
+};
+
+export default NxFormPlugin;
